6cf1bc9a98146ffae1d0244912df71c4d4f18cc2
Adgo-Simranjit <SIMRANJIT.SINGH@sapiens.com>
2025-11-06 16:41:58 +0530

    Add files via upload

---
+++ Code/WepApi/Program.cs
@@ -0,0 +1,51 @@
+
+namespace ExcelToJsonAgentApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            // Configure CORS policy
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularApp", policy =>
+                {
+                    policy.WithOrigins("http://localhost:4200")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.UseCors("AllowAngularApp");
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
